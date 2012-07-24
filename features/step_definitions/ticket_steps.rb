d41e1841be09e77717e3fe55a469222ae9b0fa58
Yeehaa <medialoperations@gmail.com>
2012-07-24 20:40:13 +0200

    add tickets are attributed to creator

---
+++ features/step_definitions/ticket_steps.rb
@@ -0,0 +1,6 @@
+Given /^"(.*?)" has created a ticket for this project:$/ do |email, table|
+  table.hashes.each do |attributes|
+  	attributes = attributes.merge!(user: User.find_by_email!(email))
+  	@project.tickets.create!(attributes)
+  end
+end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,5 +1,18 @@
 Given /^there are the following users:$/ do |table|
   table.hashes.each do |attributes|
+  	unconfirmed = attributes.delete("unconfirmed") == "true"
   	@user = User.create!(attributes)
+  	@user.confirm! unless unconfirmed
   end
+end
+
+Given /^I am signed in as them$/ do
+	steps(%Q{
+		And I am on the homepage
+		When I follow "Sign in"
+		And I fill in "Email" with "user@ticketee.com"
+		And I fill in "Password" with "password"
+		And I press "Sign in"
+		Then I should see "Signed in successfully."
+	})
 end
