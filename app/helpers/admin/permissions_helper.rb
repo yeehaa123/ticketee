b1622268a8df6762bf55dc9be9c7505c4925eb6c
Yeehaa <medialoperations@gmail.com>
2012-07-31 11:17:41 +0200

    only users with the 'change states' permission can change states

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -3,6 +3,7 @@ module Admin::PermissionsHelper
 		{ "view" 						=> "View",
 			"create tickets" 	=> "Create Tickets",
 			"edit tickets" 		=> "Edit Tickets",
-			"delete tickets" 	=> "Delete Tickets" }
+			"delete tickets" 	=> "Delete Tickets",
+			"change states"		=> "Change States" }
 	end
 end
