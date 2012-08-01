47e199341881677adc93abbd1df1a3c416d9b247
Yeehaa <medialoperations@gmail.com>
2012-08-01 10:53:32 +0200

    add remove tag functionality

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   belongs_to :state
-  attr_accessible :description, :user, :title, :assets_attributes
+  attr_accessible :description, :user, :title, :assets_attributes, :tags
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10 }
